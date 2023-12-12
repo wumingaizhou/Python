6edca656da237e56156c7250d3bd2583f669cc54
周有瑜 <140616237+wumingaizhou@users.noreply.github.com>
2023-12-12 23:08:57 +0800

    Update 99乘法表.py

--- 99乘法表.py
+++ 99乘法表.py
@@ -1,6 +1,6 @@
 # 打印9*9乘法表
-# 行
-i = 1
+# 第一版本
+i = 1 # 行
 while i <= 9:
     # 列
     j = 1
@@ -9,3 +9,10 @@ while i <= 9:
         j += 1
     print() # 换行
     i += 1
+
+
+# 第二版本
+for i in range(1,10):
+    for j in range(1,i + 1):
+        print(f"{j}*{i}={j * i}\t",end="")
+    print()
